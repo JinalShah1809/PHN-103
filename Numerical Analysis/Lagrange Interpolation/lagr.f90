43d5f2c27e8ddd8408ec4dcc6c7cb0903ab716e9
Jinal Shah <75725460+JinalShah1809@users.noreply.github.com>
2021-02-02 13:12:17 +0530

    Update lagr.f90

--- Numerical Analysis/Lagrange Interpolation/lagr.f90
+++ Numerical Analysis/Lagrange Interpolation/lagr.f90
@@ -2,17 +2,19 @@ program lagr
 use numericalanalysis
 implicit none
 
+  real :: xx, lagr
+  integer :: i,j,n
+  real, dimension (:), allocatable :: x, y
+  
+  open(unit=1,file='lagrin.dat',status='old')
+    read(1,*)n,xx
+    print *, n, xx
+    allocate (x(n), y(n))
+    read(1,*)(x(i),y(i),i=1,n)
 
-real :: xx, lagr
-integer :: i,j,n
-real, dimension (:), allocatable :: x, y
-open(unit=1,file='lagrin.dat',status='old')
-read(1,*)n,xx
-print *, n, xx
-allocate (x(n), y(n))
-read(1,*)(x(i),y(i),i=1,n)
+  lagr = lagrange(x,y,n,xx)
 
-lagr = lagrange(x,y,n,xx)
-open(unit=2,file='lagrout.txt',status='replace')
-write (2,*) lagr
+  open(unit=2,file='lagrout.txt',status='replace')
+    write (2,*) lagr
+    
 end program lagr
